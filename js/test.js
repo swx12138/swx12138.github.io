78f90e50aa0b03da1aeabcb8319ae702c77a1307
zhaosifsnsd <zhaolc_bzh@163.com>
2020-06-18 16:19:21 +0800

    2020.6.18

---
+++ js/test.js
@@ -0,0 +1,25 @@
+﻿var tm_run = true;
+
+function OnClick_() {
+    if (tm_run) {
+        document.getElementById("fgx").innerHTML = Date();
+        console.log("fgx");
+    }
+}
+
+function OnClick__() {
+    tm_run = !tm_run;
+    console.log(tm_run);
+}
+
+function ReadOut(obj) {
+    //alert(obj)
+    alert($(#tips))
+    c = obj.getAttribute("class");
+    if (c == "tips") {
+        obj.append("<p>让我康康</p>");
+    }
+    else {
+        console.log(c);
+    }
+}
