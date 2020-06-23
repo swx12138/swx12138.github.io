43095d14fa6f8368c909d818989739a2385da4fa
zhaosifsnsd <zhaolc_bzh@163.com>
2020-06-23 09:08:31 +0800

    update all

--- js/test.js
+++ js/test.js
@@ -1,25 +1,41 @@
-﻿var tm_run = true;
-
-function OnClick_() {
+﻿
+function onHeadBtnClick_dw()
+ {
     if (tm_run) {
         document.getElementById("fgx").innerHTML = Date();
         console.log("fgx");
     }
 }
 
-function OnClick__() {
-    tm_run = !tm_run;
-    console.log(tm_run);
+function onHeadBtnClick_addEvent()
+{
+    let newTip = document.createElement("div");
+    newTip.className = "tips";
+    //newTip.classList.add("tips");
+
+    newTip.appendChild(document.createTextNode("111111"));
+    //document.body.insertBefore(newTip,document.getElementById("footer"));
+    document.body.insertBefore(newTip,$("footer").get(0));
+
 }
 
-function ReadOut(obj) {
-    //alert(obj)
-    alert($(#tips))
-    c = obj.getAttribute("class");
+function onTipsClick_ly(obj)
+ {
+    //console.log(obj.id);
+    let newElem = document.createElement("p");
+    let elemContent = document.createTextNode("让我康康!(振声)"); 
+    newElem.appendChild(elemContent);
+    obj.appendChild(newElem);
+/*     c = obj.getAttribute("class");
     if (c == "tips") {
         obj.append("<p>让我康康</p>");
     }
     else {
         console.log(c);
-    }
+    } */
 }
+
+$("form").submit(function(_this = this){
+    console.log(_this);
+    alert("提交");
+});
